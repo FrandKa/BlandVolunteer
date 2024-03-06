6fa91c2f7c756ecf38631bdfede81945beb11e6a
FrandKa <acollman711@gmail.com>
2024-03-06 15:49:18 +0800

    Initial Commit

---
+++ BlandVolunteer/BlandVolunteerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlandVolunteerApp.swift
+//  BlandVolunteer
+//
+//  Created by FrandKa on 2024/3/6.
+//
+
+import SwiftUI
+
+@main
+struct BlandVolunteerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
